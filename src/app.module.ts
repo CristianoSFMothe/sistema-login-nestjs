450d84cc9d73f0e2b040fa7c328d22c279578bfa
Cristiano Ferreira <cristianodevsystem@gmail.com>
2023-01-01 10:31:09 -0300

    :rocket: chore(user): criação do resourcer de usuário

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppModule } from './user/app/app.module';
+import { UserModule } from './app/user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AppModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
